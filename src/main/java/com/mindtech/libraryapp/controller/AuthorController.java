49c1f43441dfdd503ac34f26741595fd52cf0680
Ali Baran Demir <alibarandemir798@gmail.com>
2025-07-03 12:46:35 +0300

    author feature is completed

--- src/main/java/com/mindtech/libraryapp/controller/AuthorController.java
+++ src/main/java/com/mindtech/libraryapp/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.mindtech.libraryapp.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +13,16 @@ import com.mindtech.libraryapp.service.AuthorService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+
 @RestController
 @RequestMapping("/api/authors")
+@RequiredArgsConstructor
 public class AuthorController {
 
     private final AuthorService authorService;
 
+    //Yazarları getirirken onlara ait kitapları da getiriyorum
+    @GetMapping
     public ResponseEntity<ResponseDto<List<AuthorDto>>> getAllAuthors(){
         return ResponseEntity.ok(authorService.getAllAuthors());
     }
